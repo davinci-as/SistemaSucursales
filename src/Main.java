93cc6a4758ad0705051a2cf61b18099c23b03add
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-05-19 11:39:59 -0300

    feat: agregar 3 entidades bancarias

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import vigilancia.sistema.EntidadBancaria;
+import vigilancia.sistema.Sucursal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EntidadBancaria bancoDV = new EntidadBancaria();
+        bancoDV.setNombre("Banco DV");
+        bancoDV.setDomicilio("Calle 123, BA");
+        bancoDV.setCodigo(192);
+        //Manera 1
+        Sucursal sucursal = new Sucursal();
+        bancoDV.addSucursal(sucursal);
+        //Manera 2
+        bancoDV.addSucursal(new Sucursal());
+        //Manera 3
+        bancoDV.addSucursal(190, "Calle 456, BA");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        bancoDV.getSucursales();
     }
 }
