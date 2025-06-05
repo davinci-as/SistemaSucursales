bfa793593e7c28a2c909f8b641e4609a2a1e2e10
sergiomedina-dv <131675904+sergiomedina-dv@users.noreply.github.com>
2025-06-05 12:29:00 +0000

    feat: agregar la clase Team

---
+++ src/dv/torneo/Team.java
@@ -0,0 +1,10 @@
+class Team {
+    private String nombre;
+
+    public String getName() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
